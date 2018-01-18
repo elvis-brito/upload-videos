2a728d831ba6015f85e21927bdaa39b5fd450043
Rafael Silva <rafael@rafael.nidotecnologia.com.br>
2018-01-18 12:29:24 -0200

    Modificando o visual da aplicação

--- appplex.php
+++ appplex.php
@@ -1,6 +1,8 @@
 <?php
+	
 	$uploaddir = '/data/www/media/';
 	if(rename($_FILES['arquivo']['tmp_name'] , $uploaddir.$_FILES['arquivo']['name'])){
+		chmod ($uploaddir.$_FILES['arquivo']['name'], 0777);
 		echo "Enviado!";
 	}else{
 		echo "Nao enviado!";
